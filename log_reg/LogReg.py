fixed gradient, added testing classifiactor

log_reg/LogReg.py
@@ -13,6 +13,9 @@ class LogReg:
         self.W = np.random.randn(1, 2) * 0.01
         self.b = np.random.randn(1) * 0.01
 
+    def classify(self, X):
+        return np.round(self.pred(X))
+
     def pred(self, X):
         return self.__pred(self.W, X, self.b)
 
@@ -24,8 +27,8 @@ class LogReg:
         return -np.sum(Z.reshape(Z.size, 1) * np.log(Y) + (1 - Z.reshape(Z.size, 1)) * np.log(1 - Y), 0)
 
     def __grad(self, X, Z, W, b):
-        dW = np.sum((logistic.cdf(np.dot(X, W.T) - Z.reshape(Z.size, 1)) * X))
-        db = np.sum((logistic.cdf(b - Z.reshape(Z.size, 1)) * X))
+        dW = np.sum((self.__pred(W, X, b) - Z.reshape(Z.size, 1)) * X, 0)
+        db = np.sum((self.__pred(W, X, b) - Z.reshape(Z.size, 1)), 0)
         return dW, db
 
     def train(self):
@@ -39,15 +42,15 @@ class LogReg:
         return train_loss
 
     def plot_decision_boundary(self, X, Z):
-        fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-        ax.scatter(X[:, 0], X[:, 1], c=Z, cmap=plt.cm.cool)
-        ax.set_autoscale_on(False)
-
+        # fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+        plt.scatter(X[:, 0], X[:, 1], c=Z, cmap=plt.cm.cool)
         a = - self.W[0, 0] / self.W[0, 1]
         xx = np.linspace(-30, 30)
+        plt.autoscale(False)
+
         yy = a * xx - (self.b[0]) / self.W[0, 1]
 
-        ax.plot(xx, yy)
+        plt.plot(xx, yy, '-')
         plt.interactive(False)
         plt.show()
 